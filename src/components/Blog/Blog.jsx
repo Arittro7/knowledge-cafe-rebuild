29a300cea518d198756f90646db359dfed0706d2
Arittro7 <arittro007@gmail.com>
2024-05-22 16:56:50 +0600

    blog data and layout

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,20 +1,37 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import "./blog.css";
 
-const Blog = ({blog}) => {
-
-  const {cover_img, title} = blog;
+const Blog = ({ blog }) => {
+  const { cover_img, title, author_img, author, posted_date, reading_time, hashtags } = blog;
 
   return (
-    <div>
-      <img src={cover_img} alt="Cover Image" />
-      <h2>Title: {title}</h2>
-
+    <div className="blog_container">
+      <img className="w-[800px]" src={cover_img} alt="Cover Image" />
+  
+      <div className="flex items-center justify-between">
+        <div className="flex "> 
+          <img className='w-14 rounded-full ' src={author_img} alt="author Image" />
+          <div className="ml-3">
+            <h2 className="text-2xl font-semibold">{author}</h2>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-5xl font-semibold">{title}</h2>
+      <p>
+        {
+          hashtags.map((hash, idx) => <span className="mr-2" key={idx}><a href="">#{hash}</a></span>)
+        }
+      </p>
     </div>
   );
 };
 
-Blog.propTypes ={
-  blog:PropTypes.object.isRequired
-}
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+};
 
 export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -17,7 +17,7 @@ const Blogs = () => {
     <div className='md: w-2/3'>
     <h1 className='text-5xl'>Blogs</h1>
     {
-      blogs.map(blog =><Blog blog={blog}></Blog>)
+      blogs.map(blog =><Blog key={blog.id} blog={blog}></Blog>)
     }
     </div>
   );
